9f0c9d7d4570e3e580235531104cde343133cecb
vislavathnareshkumar <vislavathnareshkumar52@gmail.com>
2023-07-27 19:57:32 +0000

    solution

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 
 const path = require("path");
 
@@ -25,3 +26,48 @@ const intializeDBAndServer = async () => {
 };
 
 intializeDBAndServer();
+
+app.get("/players/", async (request, response) => {
+  const getPlayers = `
+    SELECT 
+    *
+    FROM 
+    cricket_team`;
+
+  const playerList = await db.all(getPlayers);
+
+  const ans = (playerList) => {
+    return {
+      playerId: playerList.player_id,
+      playerName: playerList.player_name,
+      jerseyNumber: playerList.jersey_number,
+      role: playerList.role,
+    };
+  };
+
+  const player = playerList.map((eachplayer) => ans(eachplayer));
+
+  response.send(player);
+});
+
+app.post("/players/", async (request, response) => {
+  const playerDetail = request.body;
+  const { playerName, jerseyNumber, role } = playerDetail;
+
+  const addplayer = `
+
+    
+    INSERT INTO 
+    cricket_team
+    (player_name, jersey_number, role)
+    VALUES ('${playerName}', ${jerseyNumber}, '${role}')
+    
+    
+    
+    `;
+
+  const result = await db.run(addplayer);
+
+  response.send(result);
+  console.log(result);
+});
